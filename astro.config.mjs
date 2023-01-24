b9995a7375658616a429af12b84fe176640befce
kkopp001 <102523668+kkopp001@users.noreply.github.com>
2023-01-24 20:02:32 +0100

    changes added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.astro-start.netlify.com",
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: "file",
+  },
+});
