580978c517e04a5be1690c82d2b03ea1f4feff7b
kkopp001 <102523668+kkopp001@users.noreply.github.com>
2023-01-26 14:30:35 +0100

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.astro-start.netlify.com",
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: "file",
+    format: "file"
   },
+  integrations: [preact()]
 });
